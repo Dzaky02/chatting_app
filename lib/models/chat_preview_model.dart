645904a14d5c26212e2fda159c83c9621dbea40a
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-06-01 12:51:48 +0700

    Create chat preview model.

---
+++ lib/models/chat_preview_model.dart
@@ -0,0 +1,13 @@
+class ChatPreviewModel {
+  String imageUrl;
+  String name;
+  String message;
+  String time;
+
+  ChatPreviewModel({
+    required this.imageUrl,
+    required this.name,
+    required this.message,
+    required this.time,
+  });
+}
