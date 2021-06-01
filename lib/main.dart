e2f7dd8e509329e7ae79a99f29088d3a762d772f
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-06-01 10:06:54 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChattingApp());
+}
+
+class ChattingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
